e0918aedb788c9a85ce198f9b978c88219b00df7
anthilldev <55480266+anthilldev@users.noreply.github.com>
2024-08-05 16:23:41 -0500

    Update UnderConstruction.jsx

--- src/app/(components)/construction/UnderConstruction.jsx
+++ src/app/(components)/construction/UnderConstruction.jsx
@@ -6,10 +6,10 @@ const UnderConstruction = () => {
         <div className=" grid m-auto place-self-center text-white">
             <h3 className="text-lg font-bold mb-6 "> THIS APP IS CURRENTLY UNDER CONSTRUCTION</h3>
             <div>
-                <p className="font-bold mb-7">we are sorry for any inconvience this may cause</p>
+                <p className="font-bold mb-7">we are sorry for any inconvenience this may cause</p>
             </div>
             <div>
-                <p>You can Contact the owner of this application for a limited time at 832-756-4906 for any concerns regarding your services you want to request</p>
+                <p>You can contact the owner of this application for a limited time at 830-581-0612 for any concerns regarding the services you want to request</p>
             </div>
         </div>
     </div>

9999ea1b3af292346497ecae1c32e0f520b4ff0f
anthilldev <55480266+anthilldev@users.noreply.github.com>
2024-08-05 14:19:58 -0500

    v0.00.0

---
+++ src/app/(components)/construction/UnderConstruction.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UnderConstruction = () => {
+  return (
+    <div className="flex min-w-screen min-h-screen bg-slate-900">
+        <div className=" grid m-auto place-self-center text-white">
+            <h3 className="text-lg font-bold mb-6 "> THIS APP IS CURRENTLY UNDER CONSTRUCTION</h3>
+            <div>
+                <p className="font-bold mb-7">we are sorry for any inconvience this may cause</p>
+            </div>
+            <div>
+                <p>You can Contact the owner of this application for a limited time at 832-756-4906 for any concerns regarding your services you want to request</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UnderConstruction
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import UnderConstruction from "./(components)/construction/UnderConstruction";
+
+export default function Home() {
+  return (
+    <UnderConstruction />
+  );
+}
